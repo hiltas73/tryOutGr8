218f1c6928cc4fa13aeb9eb955fa134495f44ea4
hiltas73 <h_iltas@yahoo.com>
2021-07-30 23:24:52 +0300

    2nd push

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -3,5 +3,6 @@ public class test1 {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello4");
     }
 }
